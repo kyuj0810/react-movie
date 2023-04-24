ddba11894c37b333e0926fc6b1f716415e76404e
jin <kyuj0810@gmail.com>
2023-04-24 22:00:49 +0900

    디테일 완성

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,24 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Detail() {
   const { id } = useParams()
+  const [movie, setMovie] = useState([])
   const getMovie = async () => {
     const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json()
-    console.log(json)
+    console.log(json.data.movie)
+    setMovie(json.data.movie)
   }
   useEffect(() => {
     getMovie()
   }, [])
-  return <h1>Detail</h1>
+  return (
+    <div>
+      <img src={movie.large_cover_image} alt={movie.title} />
+      <h2>{movie.title}</h2>
+    </div>
+  )
 }
 export default Detail
